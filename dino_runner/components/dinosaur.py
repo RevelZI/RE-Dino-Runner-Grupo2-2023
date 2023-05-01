ebed3675c3fe52aa5b9d7d1e0269a28a629c95f1
RevelZI <revelzi345@gmail.com>
2023-05-01 17:56:07 -0400

    reduccion
    reducción

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -59,7 +59,7 @@ class Dinosaur(Sprite):
         self.image = JUMPING
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel * 4
-            self.jump_vel = self.jump_vel - 0.8
+            self.jump_vel -= 0.8
 
         if self.jump_vel < - self.JUMP_VEL:
             self.dino_rect.y = self.Y_POS
